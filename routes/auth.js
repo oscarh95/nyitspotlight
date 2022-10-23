e6fcccd8ba8c699c6b241e50fa38fa18fc312ed3
Ho-oh <ohou@nyit.edu>
2022-10-23 19:08:55 -0400

    some components and messages+conversation setup

--- routes/auth.js
+++ routes/auth.js
@@ -2,16 +2,32 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/UserModel')
 
+//register
+router.post('/register', async (req, res) => {
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
 
-router.get('/register', async (req, res) => {
-    const user = await new User({
-        username: "David Martinzez",
-        email: "dave@gmail.com",
-        password: "choom90"
-    });
-
-    await user.save();
-    res.send("user logged")
+        try{
+            const user = await newUser.save();
+            res.status(200).json(user);
+        }catch(err){
+            res.status(500).json(err);
+        }
 })
+
+//login
+router.post('/login', async (req, res) => {
+    try{
+        const user = await User.findOne({email:req.body.email});
+        !user && res.status(404).send("User not found")
+        
+        res.status(200).json(user);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
